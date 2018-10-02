d15facfaecc67ce0b62d1e33e29e875908bbf917
Rwoltx <richardx1016@gmail.com>
2018-10-02 12:47:22 -0400

    1st commit

---
+++ ShoppingList/ShoppingList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShoppingList
+//
+//  Created by Rick Wolter on 10/2/18.
+//  Copyright © 2018 RNWolter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
